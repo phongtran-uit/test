387eaf0f8646ef0010ac37d101ca3a0f3fd44cb5
Thanh Phong <phonng.tran.uit@gmail.com>
2021-04-27 11:32:37 +0700

    update

--- index.js
+++ index.js
@@ -7,30 +7,34 @@ let constraintObj = {
   },
 };
 
-//handle older browsers that might implement getUserMedia in some way
-// if (navigator.mediaDevices === undefined) {
-//     navigator.mediaDevices = {};
-//     navigator.mediaDevices.getUserMedia = function(constraintObj) {
-//         let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-//         if (!getUserMedia) {
-//             return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
-//         }
-//         return new Promise(function(resolve, reject) {
-//             getUserMedia.call(navigator, constraintObj, resolve, reject);
-//         });
-//     }
-// }else{
-//     navigator.mediaDevices.enumerateDevices()
-//     .then(devices => {
-//         devices.forEach(device=>{
-//             console.log(device.kind.toUpperCase(), device.label);
-//             //, device.deviceId
-//         })
-//     })
-//     .catch(err=>{
-//         console.log(err.name, err.message);
-//     })
-// }
+// handle older browsers that might implement getUserMedia in some way
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+  navigator.mediaDevices.getUserMedia = function (constraintObj) {
+    let getUserMedia =
+      navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    if (!getUserMedia) {
+      return Promise.reject(
+        new Error("getUserMedia is not implemented in this browser")
+      );
+    }
+    return new Promise(function (resolve, reject) {
+      getUserMedia.call(navigator, constraintObj, resolve, reject);
+    });
+  };
+} else {
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then((devices) => {
+      devices.forEach((device) => {
+        console.log(device.kind.toUpperCase(), device.label);
+        //, device.deviceId
+      });
+    })
+    .catch((err) => {
+      console.log(err.name, err.message);
+    });
+}
 let totalTime = 0;
 let flag = true;
 const renderTotalTime = (flag) => {
